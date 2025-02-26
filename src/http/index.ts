895deaae12d2e764bdf6c1d9de7cc0c0640d761f
white <grigsgt@gmail.com>
2025-02-26 21:25:50 +0300

    add interceptors to update token automatically

--- src/http/index.ts
+++ src/http/index.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
-import {getStoredToken} from "@/components/auth.tsx";
+import {getStoredToken, setStoredToken} from "@/components/auth.tsx";
+import {IAuthResponse} from "@/models/auth.ts";
 
 const api = axios.create({
     withCredentials: true,
@@ -9,6 +10,25 @@ const api = axios.create({
 api.interceptors.request.use((config) => {
     config.headers.Authorization = `Bearer ${getStoredToken()}`;
     return config;
+})
+
+api.interceptors.response.use((config) => {
+    return config;
+}, async (error) => {
+    const originalRequest = error.config;
+    if (error.response && error.response.status === 401 && !error.config.isRetry) {
+        originalRequest.isRetry = true
+        try {
+            const response = await axios.get<IAuthResponse>('http://localhost:3000/api/auth/refresh', {
+                withCredentials: true
+            })
+            setStoredToken(response.data.user.accessToken)
+            return api.request(originalRequest)
+        } catch (e) {
+            console.log('401')
+        }
+    }
+    throw error;
 })
 
 export default api;
