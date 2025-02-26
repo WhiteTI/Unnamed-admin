51497cc6e226fd03d04178702412371889e463ad
white <grigsgt@gmail.com>
2025-02-26 21:24:33 +0300

    create methods to update user data

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -2,6 +2,18 @@ import api from "@/http/index.ts";
 
 export default class UserService {
     static async fetchUsers() {
-        return api.get('/users');
+        return api.get('/api/users');
+    }
+
+    static async updateUser(id: string, newLogin?: string, newUsername?: string) {
+        return api.patch(`/api/users/${id}`, {newLogin, newUsername})
+    }
+
+    static async updateUserPassword(id: string, password: string, newPassword: string) {
+        return api.patch(`/api/users/${id}`, {password, newPassword})
+    }
+
+    static async updateUserImage(id: string, newImage?: File,) {
+        return api.patch(`/api/users/${id}`, {newImage})
     }
 }
