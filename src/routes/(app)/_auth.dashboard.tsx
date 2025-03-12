d72d27c239cb0e4d6919fb7eb9428a4424292f7a
white <grigsgt@gmail.com>
2025-03-12 22:18:56 +0300

    add bg color

--- src/routes/(app)/_auth.dashboard.tsx
+++ src/routes/(app)/_auth.dashboard.tsx
@@ -13,7 +13,7 @@ function DashBoard() {
         <SidebarProvider>
             <AppSidebar/>
             <SidebarInset>
-                <header className='flex h-16 shrink-0 items-center gap-2 border-b px-4 sticky top-0'>
+                <header className='flex h-16 shrink-0 items-center gap-2 border-b px-4 sticky top-0 bg-background z-10'>
                     <SidebarTrigger/>
                     <Separator orientation='vertical' className='mr-2 h-4'/>
                     <div>(～￣▽￣)～</div>
