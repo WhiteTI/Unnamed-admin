066524555b27f1b15b9fd7094e8f429639b0cfad
white <grigsgt@gmail.com>
2025-03-22 22:50:17 +0300

    add logo

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -7,13 +7,17 @@ import {
 import NavUser from "@/components/nav-user.tsx";
 import NavGames from "@/components/nav-games.tsx";
 import NavMenu from "@/components/nav-menu.tsx";
+import logo from '@/assets/img/logo.jpg'
 
 const AppSidebar = () => {
 
     return (
-        <Sidebar collapsible='icon'>
-            <SidebarHeader>
-                Logo + Название
+        <Sidebar collapsible='offcanvas'>
+            <SidebarHeader className='flex-row items-center gap-3'>
+                <div className='w-14 h-14 rounded-2xl overflow-hidden'>
+                    <img src={logo} alt="logo"/>
+                </div>
+                <span className='text-xl font-medium'>404 Not Found</span>
             </SidebarHeader>
 
             <SidebarContent>
